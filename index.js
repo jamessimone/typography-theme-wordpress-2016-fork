d2e220af228803fc20d0db71e9a9f117d80e6ce2
james simone <jamirus99@gmail.com>
2019-05-29 04:55:05 -0400

    Removing google font section and no longer over-riding H1 font family with Montserrat due to lack of Cyrillic character support in Montserrat base font family

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-import gray from "gray-percentage"
-import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants"
+import gray from "gray-percentage";
+import { MOBILE_MEDIA_QUERY } from "typography-breakpoint-constants";
 
 const theme = {
   title: "Wordpress Theme 2016",
   baseFontSize: "16px",
   baseLineHeight: 1.75,
   scaleRatio: 5 / 2,
-  googleFonts: [
-    {
-      name: "Montserrat",
-      styles: ["700"],
-    },
-    {
-      name: "Merriweather",
-      styles: ["400", "400i", "700", "700i", "900", "900i"],
-    },
-  ],
   headerFontFamily: ["Merriweather", "Georgia", "serif"],
   bodyFontFamily: ["Merriweather", "Georgia", "serif"],
   bodyColor: "hsla(0,0%,0%,0.9)",
@@ -23,66 +13,63 @@ const theme = {
   bodyWeight: 400,
   boldWeight: 700,
   overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
-    h1: {
-      fontFamily: ["Montserrat", "sans-serif"].join(","),
-    },
     blockquote: {
       ...scale(1 / 5),
       color: gray(41),
       fontStyle: "italic",
       paddingLeft: rhythm(13 / 16),
       marginLeft: rhythm(-1),
-      borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`,
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(10)}`
     },
     "blockquote > :last-child": {
-      marginBottom: 0,
+      marginBottom: 0
     },
     "blockquote cite": {
       ...adjustFontSizeTo(options.baseFontSize),
       color: options.bodyColor,
-      fontWeight: options.bodyWeight,
+      fontWeight: options.bodyWeight
     },
     "blockquote cite:before": {
-      content: '"— "',
+      content: '"— "'
     },
     ul: {
-      listStyle: "disc",
+      listStyle: "disc"
     },
     "ul,ol": {
-      marginLeft: rhythm(1),
+      marginLeft: rhythm(1)
     },
     [MOBILE_MEDIA_QUERY]: {
       blockquote: {
         marginLeft: rhythm(-3 / 4),
         marginRight: 0,
-        paddingLeft: rhythm(9 / 16),
-      },
+        paddingLeft: rhythm(9 / 16)
+      }
     },
     "h1,h2,h3,h4,h5,h6": {
-      marginTop: rhythm(2),
+      marginTop: rhythm(2)
     },
     h4: {
       letterSpacing: "0.140625em",
-      textTransform: "uppercase",
+      textTransform: "uppercase"
     },
     h6: {
-      fontStyle: "italic",
+      fontStyle: "italic"
     },
     a: {
       boxShadow: "0 1px 0 0 currentColor",
       color: "#007acc",
-      textDecoration: "none",
+      textDecoration: "none"
     },
     "a:hover,a:active": {
-      boxShadow: "none",
+      boxShadow: "none"
     },
     "mark,ins": {
       background: "#007acc",
       color: "white",
       padding: `${rhythm(1 / 16)} ${rhythm(1 / 8)}`,
-      textDecoration: "none",
-    },
-  }),
-}
+      textDecoration: "none"
+    }
+  })
+};
 
-export default theme
+export default theme;
